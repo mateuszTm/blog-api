ba23dfe8fb9ce1d8bda5957e856fe32c1ef61da2
Mateusz <mateo_pl@hotmail.com>
2020-01-26 15:48:33 +0100

    poczatek

---
+++ src/main/java/appbeta/blog/controller/User.java
@@ -0,0 +1,12 @@
+package appbeta.blog.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@RestController
+@RequestMapping("/user")
+public class User {
+
+}
