cb56182f970b7b53a1c93bf406677b91f45fa905
Mateusz <mateo_pl@hotmail.com>
2020-05-01 21:46:39 +0200

    auth jwt server

--- src/main/java/appbeta/blog/config/SecurityConfig.java
+++ src/main/java/appbeta/blog/config/SecurityConfig.java
@@ -22,9 +22,10 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
-import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+//import org.springframework.security.oauth2.provider.error.OAuth2AccessDeniedHandler;
+//import org.springframework.security.oauth2.provider.token.TokenStore;
+//import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+//import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -32,35 +33,20 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
-@EnableWebSecurity
-@Order(1)
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
-	
-	@Autowired
-	@Qualifier("userServiceImpl")
-	private UserDetailsService userDetailsService;
-	
-	@Override
-	public void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(userDetailsService);
-	}
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
-	}	
+	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.anonymous().disable()
+		http.cors().and()
 			.authorizeRequests()
+				.antMatchers("/post").permitAll()
 				.anyRequest().authenticated().and()
-			.formLogin();
+			.oauth2ResourceServer()
+				.jwt();
 	}
-		
-	@Override
-    @Bean
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
 }
