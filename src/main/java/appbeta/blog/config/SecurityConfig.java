881ccd38eb0892abdddc900fa07c421a29891e26
Mateusz <mateo_pl@hotmail.com>
2020-05-05 16:03:33 +0200

    dodano obsluge cors

--- src/main/java/appbeta/blog/config/SecurityConfig.java
+++ src/main/java/appbeta/blog/config/SecurityConfig.java
@@ -16,7 +16,7 @@ import java.util.Collections;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -49,4 +49,5 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.oauth2ResourceServer()
 				.jwt();
 	}
+	
 }
